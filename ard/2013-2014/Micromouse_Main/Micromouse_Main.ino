4d60a491a1572b57ee931cd25b15da911d963764
Tomasz <tomaszpi@buffalo.edu>
2014-11-02 10:45:26 -0500

    Added Correct Arduino
    Added the most updated Arduino files on my computer, which we did
    development on for a large portion of the time. Hopefully correct?

--- ard/2013-2014/Micromouse_Main/Micromouse_Main.ino
+++ ard/2013-2014/Micromouse_Main/Micromouse_Main.ino
@@ -1,13 +1,7 @@
-#include <StandardCplusplus.h>
-#include <system_configuration.h>
-#include <unwind-cxx.h>
-#include <utility.h>
-
 #include <avr/pgmspace.h> // Needed in order to recall accel table out of flash memory to save space
 
-#include "FloodFill.h"
 /*
-  Notes: Tab order counts. variables declared in tabs only are accesible in later tabs
+  Notes: Tab order counts. variables declared in tabs only are accesible in ltaer tabs
          This does not pertain to functions.  Functions in any tab can be used anywhere
          Put _XX_ before file names to order them by number XX. the leading _ is important
 */
@@ -16,32 +10,15 @@ void setup(){
   Serial.begin(9600);
   setupPins();
   setupTimers();
+  delay(7500);
   attachInterrupt(5, leftDig, RISING);
   attachInterrupt(4, rightDig, RISING);
-  delay(3000);
-  FloodFill f;
-  
-  f.solve();
 }
 
 void loop(){
   //centerFirstBox();
+  solve();
+  //_moveForward(3);
   //_turnLeft();
-  //turnRightSteps(38);
-  //solve();
-  //_moveForward(1);
-  //_turnLeft();
-
-}
-
-void _resetHonored(){
-}
-
-boolean _resetRequested(){
-  return false;
-}
-
-void _turnAround(){
-  _turnRight(2);
-  //_turnRight();
+  delay(1000);
 }
--- ard/2013-2014/Micromouse_Main/_01_Control.ino
+++ ard/2013-2014/Micromouse_Main/_01_Control.ino
@@ -1,44 +1,205 @@
-bool _undoRequested(){
-  ////Eriralprintln("*********UNDO REQUESTED************");
-  return resetRunRequested;
-}
-// button is 5
-// led is 2
-void _undoHonored(){
-  ////Eriralprintln("*********UNDO HONORED************");
-  while(!digitalRead(5))
-    delay(1);
-  delay(500);
-  digitalWrite(2, LOW);
-  ////Eriralprintln("*********UNDO HONORED 2************");
-  resetRunRequested = false;
-}
+void _moveForward(int boxes){
+  if(boxes == 0)
+    return;
+  Serial.println("----------------------------------------");  
+  //syncMotors();
+  Serial.print(left_stepper_position);
+  Serial.print("     ");
+  Serial.println(right_stepper_position);
+  long lastTimeStamp = timestamp; // timestamp determines 
 
-
-void _resetPosition(){
-  ////Eriral//Eriralprintln("*********RESET***********");
-  delay(1500);
-  while(digitalRead(5))
+  //while(lastTimeStamp == timestamp){ //more efficient way to make sure new sensor data is in
+  //  delay(1); 
+  //}
+  lastTimeStamp = timestamp;
+  
+  //syncMotors();
+  //preMoveTwist();
+  //syncMotors();
+  
+  //lastTimeStamp = timestamp;
+  
+  int avgRawSensorCorrect = 0;
+  int leftRawSensorCorrect = 0;
+  int rightRawSensorCorrect = 0;
+  
+  int rightSensor = 0;
+  int leftSensor = 0;
+  
+  int right1Sensor = 0;
+  int left1Sensor = 0;
+  
+  int right2Sensor = 0;
+  int left2Sensor = 0;
+  
+  digitalWrite(RIGHT_DIR_PIN, HIGH); 
+  digitalWrite(LEFT_DIR_PIN, HIGH);
+  
+  sensorAverage[LEFTFORWARDSENSOR][avgSamples +1] = 0; // set the sensor as reliable for the movement initially
+  sensorAverage[RIGHTFORWARDSENSOR][avgSamples +1] = 0; //
+  
+  stoppingFrontWall = 0; // Method mouse is using to stop in the middle of a cell
+  stoppingSideWall = 0;
+  
+  wallLeftSteps = 0; // sensor read
+  wallRightSteps = 0; // sensor read
+  leftWallTime = 0;
+  rightWallTime = 0;
+  
+  delay(80);
+  
+  syncMotors();
+  
+  right_stepper_target += FORWARD_STEPS * boxes; //
+  left_stepper_target += FORWARD_STEPS * boxes; //
+  
+  stepper_enabled = true;
+  
+  right1Sensor = sensorAverage[RIGHTFORWARDSENSOR][0];
+    left1Sensor = sensorAverage[LEFTFORWARDSENSOR][0];
+    
+    right2Sensor = sensorAverage[RIGHTFORWARDSENSOR][1];
+    left2Sensor = sensorAverage[LEFTFORWARDSENSOR][1];
+  
+  if ( ((left1Sensor <= ANGLELEFTVALUE - ALLOWEDRANGE) || (left1Sensor >= ANGLELEFTVALUE + ALLOWEDRANGE) || (left2Sensor <= ANGLELEFTVALUE - ALLOWEDRANGE) || (left2Sensor >= ANGLELEFTVALUE + ALLOWEDRANGE) || sensorAverage[2][avgSamples] >= 280) 
+         && (sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] == 0) ) // Check if sensor value is outside reliable range
+    {
+      sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] = RESETSTEPAMOUNT; // UNRELIABLE SENSOR FOR REST OF MOVE STEP COUNT AT WHICH IS WAS DECLARED UNRELIABLE
+      compensateLeft(0);
+    }
+    if ( ((right1Sensor <= ANGLERIGHTVALUE - ALLOWEDRANGE) || (right1Sensor >= ANGLERIGHTVALUE + ALLOWEDRANGE) || (right2Sensor <= ANGLERIGHTVALUE - ALLOWEDRANGE) || (right2Sensor >= ANGLERIGHTVALUE + ALLOWEDRANGE) || sensorAverage[2][avgSamples] >= 280)
+         && (sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] == 0) )
+    {
+      sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] = RESETSTEPAMOUNT; // UNRELIABLE SENSOR FOR REST OF MOVE STEP COUNT AT WHICH IS WAS DECLARED UNRELIABLE 
+      compensateLeft(0);
+    }
+    
+  delay(80);
+  //Serial.println("----------------------------------------");
+  while(stepper_enabled == true)
   {
-    digitalWrite(2, HIGH);
-    delay(50);
-    digitalWrite(2, LOW);
-    delay(50);
-  }
-  delay(3000);
-}
-
-void stopFrontWall(){
-  correct = 0;
-  while (stepper_enabled == true)
-  {
-      if(!(digitalRead(FRONT_DIGITAL)))
+    if(sensorAverage[2][avgSamples] >= FRONTSENSORWALLSTOPSTART && stoppingFrontWall == 0) // stopping if wall is present
+    {
+      right_stepper_target = right_stepper_position + FRONTSTEPSWALLSTOP; // Add number of steps remaining to wall
+      left_stepper_target = left_stepper_position + FRONTSTEPSWALLSTOP;
+      stoppingFrontWall = 1;
+    }
+    
+    right1Sensor = sensorAverage[RIGHTFORWARDSENSOR][0];
+    left1Sensor = sensorAverage[LEFTFORWARDSENSOR][0];
+    
+    right2Sensor = sensorAverage[RIGHTFORWARDSENSOR][1];
+    left2Sensor = sensorAverage[LEFTFORWARDSENSOR][1];   
+    
+    if ((sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] <= left_stepper_position))
+    {
+      sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] = 0;
+    }
+    if ((sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] <= right_stepper_position))
+    {
+      sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] = 0;
+    }
+    
+    if ( ((left1Sensor <= ANGLELEFTVALUE - ALLOWEDRANGE) || (left1Sensor >= ANGLELEFTVALUE + ALLOWEDRANGE) || (left2Sensor <= ANGLELEFTVALUE - ALLOWEDRANGE) || (left2Sensor >= ANGLELEFTVALUE + ALLOWEDRANGE) || sensorAverage[2][avgSamples] >= 280) 
+         && (sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] == 0) ) // Check if sensor value is outside reliable range
+    {
+      sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] = left_stepper_position + RESETSTEPAMOUNT; // UNRELIABLE SENSOR FOR REST OF MOVE STEP COUNT AT WHICH IS WAS DECLARED UNRELIABLE
+      compensateLeft(0);
+    }
+    if ( ((right1Sensor <= ANGLERIGHTVALUE - ALLOWEDRANGE) || (right1Sensor >= ANGLERIGHTVALUE + ALLOWEDRANGE) || (right2Sensor <= ANGLERIGHTVALUE - ALLOWEDRANGE) || (right2Sensor >= ANGLERIGHTVALUE + ALLOWEDRANGE) || sensorAverage[2][avgSamples] >= 280)
+         && (sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] == 0) )
+    {
+      sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] = right_stepper_position + RESETSTEPAMOUNT; // UNRELIABLE SENSOR FOR REST OF MOVE STEP COUNT AT WHICH IS WAS DECLARED UNRELIABLE 
+      compensateRight(0);
+    }
+    
+    rightSensor = (right1Sensor + right2Sensor) / 2;
+    leftSensor = (left1Sensor + left2Sensor) / 2;
+    
+    /*Serial.print(leftSensor);
+    Serial.print("      ");
+    Serial.println(sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1]);
+    Serial.print("                ");
+    Serial.print(rightSensor);
+    Serial.print("      ");
+    Serial.println(sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1]);*/
+    int walls = 0;
+    
+    if (sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] == 0)  // if Sensor is reliable preform data correction
+    {
+      rightRawSensorCorrect = ANGLERIGHTVALUE - rightSensor;
+      walls++;
+      if (sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] != 0)
+        leftRawSensorCorrect = rightRawSensorCorrect; // in case left is uneviable the average will still be right
+    }
+    if (sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] == 0)
+    {
+      walls++;
+      leftRawSensorCorrect = leftSensor - ANGLELEFTVALUE;
+      if (sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] != 0)
+        rightRawSensorCorrect = leftRawSensorCorrect;
+    }    
+    
+    //compensateRight(0); // Reset compensate values so the move function does not exit with them
+    //compensateLeft(0);  
+    
+    if(walls != 0)
+    {
+       avgRawSensorCorrect = (rightRawSensorCorrect + leftRawSensorCorrect) / 2;
+       Serial.print(avgRawSensorCorrect);
+       Serial.print("       -         ");
+       if (sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] == 0)
+         Serial.print(leftSensor);
+       else
+         Serial.print("   ");
+       Serial.print(",");
+       if (sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] == 0)  // if Sensor is reliable preform data correction
+         Serial.println(rightSensor);
+       else
+         Serial.println();
+       if (avgRawSensorCorrect > 0)
+       {
+         if (walls = 1)
+           compensateRight(-(avgRawSensorCorrect * TWISTMOVEMENTMULTIPLIER1WALL));
+         else
+           compensateRight(-(avgRawSensorCorrect * TWISTMOVEMENTMULTIPLIER2WALLS));
+       }
+       if (avgRawSensorCorrect < 0)
+       {
+         if (walls = 1)
+           compensateLeft((avgRawSensorCorrect * TWISTMOVEMENTMULTIPLIER1WALL));
+         else
+           compensateLeft((avgRawSensorCorrect * TWISTMOVEMENTMULTIPLIER2WALLS));
+       }
+    }
+    while(lastTimeStamp == timestamp)
+    {
+      if (stoppingFrontWall == 0 && wallLeftSteps != 0 && stoppingSideWall == 0 && abs(left_stepper_target - wallLeftSteps) <= FORWARD_STEPS)
       {
-         Estop();
-         delay(100);
-         stoppingFrontWall = 0;
+        right_stepper_target = wallLeftSteps + SIDESTEPWALLSTOP; // Add number of steps remaining to wall
+        left_stepper_target = wallLeftSteps + SIDESTEPWALLSTOP;
+        stoppingSideWall = 1;
       }
-  }
+      if (stoppingFrontWall == 0 && wallRightSteps != 0 && stoppingSideWall == 0 && abs(right_stepper_target - wallRightSteps) <= FORWARD_STEPS)
+      {
+        right_stepper_target = wallRightSteps + SIDESTEPWALLSTOP; // Add number of steps remaining to wall
+        left_stepper_target = wallRightSteps + SIDESTEPWALLSTOP;
+        stoppingSideWall = 1;
+      }
+      //delay(1); // delay to not hog cpu power
+    }
+    lastTimeStamp = timestamp;
+    if(!(digitalRead(FRONT_DIGITAL)))
+    {
+       Estop();
+       delay(100);
+       stoppingFrontWall = 0;
+    }
+  }    // END MOVEMENT FORWARD
+  
+  compensateLeft(0);
+  compensateRight(0);
+  
   if (stoppingFrontWall == 1)
   {
     //bool initialFrontValue = digitalRead(FRONT_DIGITAL);
@@ -50,121 +211,43 @@ void stopFrontWall(){
       delay(1);
     Estop();
     speed_max = temp_speed;
+    delay(50);
   }
-  stoppingFrontWall = 0;
-  delay(80);
-  syncMotors();
-  right_stepper_target += 6; //
-  left_stepper_target += 6; //
-  
-  digitalWrite(RIGHT_DIR_PIN, HIGH); 
-  digitalWrite(LEFT_DIR_PIN, HIGH);
-  
-  stepper_enabled = true;
-  
-  delay(10);
-}
-
-void _moveForward(){
-  int boxes = 1;
-  if(boxes == 0)
-    return;
-  
-  digitalWrite(RIGHT_DIR_PIN, HIGH); 
-  digitalWrite(LEFT_DIR_PIN, HIGH);
-  
-  sensorAverage[LEFTFORWARDSENSOR][avgSamples +1] = 0; // set the sensor as reliable for the movement initially
-  sensorAverage[RIGHTFORWARDSENSOR][avgSamples +1] = 0; //
-  
-  stoppingFrontWall = 0; // Method mouse is using to stop in the middle of a cell
-  stoppingSideWall = 0;
-    
-  delay(80);
-  
-  if (stepper_enabled == false)
-  {
-    syncMotors();
-  }
-  right_stepper_target += FORWARD_STEPS * boxes; //
-  left_stepper_target += FORWARD_STEPS * boxes; //
-  
-  stepper_enabled = true;
-  
-  int right1Sensor = sensorAverage[RIGHTFORWARDSENSOR][0];
-  int left1Sensor = sensorAverage[LEFTFORWARDSENSOR][0];
-    
-  int right2Sensor = sensorAverage[RIGHTFORWARDSENSOR][1];
-  int left2Sensor = sensorAverage[LEFTFORWARDSENSOR][1];
-  
-  sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] = 0;
-  sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] = 0;
-  
-  if ( ((left1Sensor <= ANGLELEFTVALUE - ALLOWEDRANGE) || (left1Sensor >= ANGLELEFTVALUE + ALLOWEDRANGE) || (left2Sensor <= ANGLELEFTVALUE - ALLOWEDRANGE) || (left2Sensor >= ANGLELEFTVALUE + ALLOWEDRANGE) || sensorAverage[2][avgSamples] >= 280) 
-         && (sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] == 0) ) // Check if sensor value is outside reliable range
-  {
-    sensorAverage[LEFTFORWARDSENSOR][avgSamples + 1] = RESETSTEPAMOUNT; // UNRELIABLE SENSOR FOR REST OF MOVE STEP COUNT AT WHICH IS WAS DECLARED UNRELIABLE
-  }
-  if ( ((right1Sensor <= ANGLERIGHTVALUE - ALLOWEDRANGE) || (right1Sensor >= ANGLERIGHTVALUE + ALLOWEDRANGE) || (right2Sensor <= ANGLERIGHTVALUE - ALLOWEDRANGE) || (right2Sensor >= ANGLERIGHTVALUE + ALLOWEDRANGE) || sensorAverage[2][avgSamples] >= 280)
-       && (sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] == 0) )
-  {
-    sensorAverage[RIGHTFORWARDSENSOR][avgSamples + 1] = RESETSTEPAMOUNT; // UNRELIABLE SENSOR FOR REST OF MOVE STEP COUNT AT WHICH IS WAS DECLARED UNRELIABLE 
-  }
-  correct = 1;
-  if (moveBuffered == 1)
-   moveBuffered = 2;
-  if(boxes != 1)
-    waitMovement();
+  delay(80);  
+  Serial.println("----------------------------------------");  
 }
 
 int _turnLeft(){
-  
-  bufferedTurn = 2;
-  waitMovement();
-  correct = 0;
-  
-  moveBuffered = 0;
-  
   digitalWrite(RIGHT_DIR_PIN, HIGH);
   digitalWrite(LEFT_DIR_PIN, LOW);
   
+  
   // turn on 1/8 microstepping
- 
+  
   digitalWrite(48, HIGH); //MS 2
   digitalWrite(51, HIGH);  //MS 2
-  
   int temp_speed = speed_max;
   int temp_accel = table_increment;
-  table_increment = temp_accel + 8;
+  table_increment = temp_accel + 6;
   speed_max = 500;
-  
   delay(2); 
   syncMotors();
-  right_stepper_target += TURN_STEPS;//79;
-  left_stepper_target += TURN_STEPS;//79;
+  right_stepper_target += TURN_STEPS;
+  left_stepper_target += TURN_STEPS;
   //delay(1); // let microstepping go into effect
   stepper_enabled = true;
-  
-  delay(1); // Apears to fix the waitMovement somehow not waiting
   waitMovement();
   
-  speed_max = temp_speed;
-  table_increment = temp_accel;
   //
+  table_increment = temp_accel;
+  speed_max = temp_speed;
   digitalWrite(48, LOW); //MS 2
   digitalWrite(51, LOW);  //MS 2
-  ////Eriral//Eriralprintln("---------------------");
-  //delay(5000);
+  
   delay(2);
 }
 
-int _turnRight(int times){
-  
-  bufferedTurn = 1;
-  waitMovement();
-  correct = 0;
-  
-  moveBuffered = 0;
-  
+int _turnRight(){
   digitalWrite(RIGHT_DIR_PIN, LOW);
   digitalWrite(LEFT_DIR_PIN, HIGH);
   
@@ -175,16 +258,15 @@ int _turnRight(int times){
   
   int temp_speed = speed_max;
   int temp_accel = table_increment;
-  table_increment = temp_accel + 8;
+  table_increment = temp_accel + 6;
   speed_max = 500;
   
   delay(2); 
   syncMotors();
-  right_stepper_target += TURN_STEPS * times;//79;
-  left_stepper_target += TURN_STEPS * times;//79;
+  right_stepper_target += TURN_STEPS;//79;
+  left_stepper_target += TURN_STEPS;//79;
   //delay(1); // let microstepping go into effect
   stepper_enabled = true;
-  
   delay(1); // Apears to fix the waitMovement somehow not waiting
   waitMovement();
   
@@ -193,8 +275,7 @@ int _turnRight(int times){
   //
   digitalWrite(48, LOW); //MS 2
   digitalWrite(51, LOW);  //MS 2
-  //Eriralprintln("---------------------");
-  //delay(5000);
+  
   delay(2);
 }
 
@@ -215,11 +296,9 @@ void syncMotors(){
 }
 
 void waitMovement(){ // only useful for turns, all move forwards attempt error correction
-  while(stepper_enabled == true)
+  while(stepper_enabled)
   {
-    if (stoppingFrontWall == 1)
-      stopFrontWall();
-    delay(1); // no sense wasting clock cycles during movement
+    delay(10); // no sense wasting clock cycles during movement
   }
   delay(100); //Make sure the wheels come to a complete time
 }
@@ -243,7 +322,6 @@ void moveSteps(int steps, bool dir){
   right_stepper_target += steps;
   left_stepper_target += steps;
   stepper_enabled = true;
-  
   delay(1);
 }
 
@@ -253,7 +331,6 @@ void turnLeftSteps(int steps){
   right_stepper_target += steps;
   left_stepper_target += steps;
   stepper_enabled = true;
-  
   waitMovement();
 }
 
@@ -263,95 +340,30 @@ void turnRightSteps(int steps){
   right_stepper_target += steps;
   left_stepper_target += steps;
   stepper_enabled = true;
-  
   waitMovement();
 }
 
 bool _wallLeft()
 {
-  if (moveBuffered == 2)
-  {
-    while(abs(right_stepper_target - right_stepper_position) >= 340)
-      delay(1);
-  }
-  if (stepper_enabled)
-  {
-    //Eriralprint("LEFT: ");
-    if (sensorAverage[RIGHTFORWARDSENSOR][0] > right_wall_min || sensorAverage[RIGHTFORWARDSENSOR][1] > right_wall_min){
-      moveBuffered = 1;
-      //Eriralprintln(1);
-      return 1;
-    }
-    else{
-      moveBuffered = 1;
-      //Eriralprintln(0);
-      return 0;
-    }
-  }
-  else
-  {
-    return !(digitalRead(LEFT_BACK_DIGITAL));
-  }
+  return !(digitalRead(LEFT_BACK_DIGITAL));
 }
 
 bool _wallRight()
 {
-  if (moveBuffered == 2)
-  {
-    while(abs(right_stepper_target - right_stepper_position) >= 340)
-      delay(1);
-  }
-  if (stepper_enabled)
-  {
-    //Eriralprint("RIGHT: ");
-    if (sensorAverage[LEFTFORWARDSENSOR][0] > left_wall_min || sensorAverage[LEFTFORWARDSENSOR][1] > left_wall_min){
-      moveBuffered = 1;
-      //Eriralprintln(1);
-      return 1;
-    }
-    else{
-      moveBuffered = 1;
-      //Eriralprintln(0);
-      return 0;
-    }
-  }
-  else
-  {
   return !(digitalRead(RIGHT_BACK_DIGITAL));
-  }
 }
 
 bool _wallFront()
 {
-  if (moveBuffered == 2)
-  {
-    while(abs(right_stepper_target - right_stepper_position) >= 340)
-      delay(1);
-  }
-  if(stepper_enabled)
-  {
-    if (sensorAverage[2][0] > front_wall_min || sensorAverage[2][1] > front_wall_min)
-    {
-      moveBuffered = 1;
-      return 1;
-    }else{
-      moveBuffered = 1;
-      return 0;
-    }
-  }
+  if (sensorAverage[2][avgSamples] > 350)
+    return 1;
   else
-  {
-    if (sensorAverage[2][avgSamples] > 350)
-      return 1;
-    else
-      return 0;
-  }
+    return 0;
 }
 
 void Estop()
 {
-  stepper_enabled = true;
-  
+  stepper_enabled = false;
   right_stepper_position = right_stepper_target;
   left_stepper_position = left_stepper_target;
   left_speed = 0;
@@ -360,7 +372,7 @@ void Estop()
   compensateLeft(0);
 }
 
-/*void preMoveTwist(){
+void preMoveTwist(){
 
   // Pre_Move Twist correction
     
@@ -389,12 +401,12 @@ void Estop()
     
    avgRawSensorCorrect = (rightRawSensorCorrect + leftRawSensorCorrect) / 2;
     
-   //if (avgRawSensorCorrect > 0)
+   if (avgRawSensorCorrect > 0)
        //turnRightSteps(avgRawSensorCorrect * TWISTMOVEMENTMULTIPLIER);
-       //Eriralprintln(avgRawSensorCorrect = 1);
-   //if (avgRawSensorCorrect < 0)
-       //Eriralprintln(avgRawSensorCorrect = 1);
+       Serial.println(avgRawSensorCorrect = 1);
+   if (avgRawSensorCorrect < 0)
+       Serial.println(avgRawSensorCorrect = 1);
        //turnLeftSteps(-(avgRawSensorCorrect * TWISTMOVEMENTMULTIPLIER));
   // End Pr-Move Twist Correction
-}*/
+}
 
