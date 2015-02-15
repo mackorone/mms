2a2fbdc9076f8392aed7e22ce50c1672ad7bf792
Mack Ward <mackorone@gmail.com>
2015-02-15 14:43:53 -0500

    Got the "correct" implementation of mouse movement, but no improvement over easy method

--- src/sim/Mouse.cpp
+++ src/sim/Mouse.cpp
@@ -104,7 +104,7 @@ void Mouse::update(const Time& elapsed) {
     // we should adjust the mouses position so that it's where it would be after moving for
     // the "elapsed" duration.
 
-    // TODO: Document this...
+    // TODO: Document this more...
 
     // Left wheel
     MetersPerSecond dtl(m_leftWheel.getAngularVelocity().getRadiansPerSecond() * m_leftWheel.getRadius().getMeters());
@@ -112,23 +112,55 @@ void Mouse::update(const Time& elapsed) {
     // Right wheel
     MetersPerSecond dtr(m_rightWheel.getAngularVelocity().getRadiansPerSecond() * m_rightWheel.getRadius().getMeters());
 
+    // TODO: SOM
+    // TODO: There's a better closed form equation for x,y position. Right now, we're under the assumption that
+    // the elapsed time is small, so that the rotation doesn't change too much. But as we're moving, our rotation
+    // changes and thus our direction changes, so this will affect trajectory.
+    // See http://rossum.sourceforge.net/papers/DiffSteer/ for derivation of the kinematic equations and more
+    // information about the more accurate closed form solution
+    // Instantaneous change in x,y is only valid for one instant (not the whole duration) so we need a better formula
+
     float BASELINE(m_rightWheel.getPosition().getX() - m_leftWheel.getPosition().getX());
+
+    if (fabs((dtr+dtl).getMetersPerSecond()) < 1) { // TODO
+        Meters distance((-0.5 * dtl.getMetersPerSecond() + 0.5 * dtr.getMetersPerSecond()) * elapsed.getSeconds());
+        m_translation = m_translation + Polar(distance.getMeters(), M_PI / 2.0 + m_rotation.getRadians()); // TODO
+        static int i = 0;
+        //std::cout << i++ << std::endl;
+    }
+    else {
+        dtl = MetersPerSecond(-1*dtl.getMetersPerSecond());
+
+        // TODO
+        float x_0 = m_translation.getY();
+        float y_0 = -1*m_translation.getX();
+
+        float x = x_0 + 
+
+                  (BASELINE*(dtr+dtl).getMetersPerSecond())
+                  /(2.0*(dtr-dtl).getMetersPerSecond())
+
+                  *(sin((dtr-dtl).getMetersPerSecond() * elapsed.getSeconds() / BASELINE + m_rotation.getRadians())
+                   -sin(m_rotation.getRadians()));
+
+        float y = y_0 - 
+
+                  (BASELINE*(dtr+dtl).getMetersPerSecond())
+                  /(2.0*(dtr-dtl).getMetersPerSecond())
+
+                  *(cos((dtr-dtl).getMetersPerSecond() * elapsed.getSeconds() / BASELINE + m_rotation.getRadians())
+                   -cos(m_rotation.getRadians()));
+
+        m_translation = Cartesian(-y, x);
+
+        dtl = MetersPerSecond(-1*dtl.getMetersPerSecond());
+    }
+
+    // TODO: This is correct
     Radians theta((dtr.getMetersPerSecond() - (-dtl.getMetersPerSecond())) / BASELINE * elapsed.getSeconds());
     m_rotation = m_rotation + theta;
 
-    /*
-    Cartesian WHEEL_CENTER(BASELINE, m_rightWheel.getPosition().getY());
-    Polar vec(WHEEL_CENTER.getRho(), m_rotation.getRadians() + WHEEL_CENTER.getTheta()); // The current displacement vector
-
-    Cartesian disp(vec.getX() - WHEEL_CENTER.getX(), vec.getY() - WHEEL_CENTER.getY());
-    */
-
-    // TODO: Direction...
-    Meters distance((-0.5 * dtl.getMetersPerSecond() + 0.5 * dtr.getMetersPerSecond()) * elapsed.getSeconds());
-    m_translation = m_translation + Polar(distance.getMeters(), M_PI / 2.0 + m_rotation.getRadians()); // TODO
-
-    //m_translation = Cartesian(m_translation.getX() + disp.getX(), m_translation.getY() + disp.getY());
-
+    
 }
 
 // TODO... better interface
