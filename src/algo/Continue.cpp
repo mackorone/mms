96ed05c42595e1a0038be5658afef145d4acaf84
Mack Ward <mackorone@gmail.com>
2015-03-17 19:19:25 -0400

    Wrote a working algorithm, turned on printouts (except for frames)

--- src/algo/Continue.cpp
+++ src/algo/Continue.cpp
@@ -2,8 +2,6 @@
 
 #include <cmath>
 
-#include <iostream> // TODO
-
 void Continue::solve(sim::MouseInterface* mouse) {
 
     // Initialize the member variable
@@ -31,33 +29,38 @@ bool Continue::wallFront() {
 
 void Continue::turnRight() {
     m_mouse->setWheelSpeeds(-5*M_PI, -5*M_PI);
-    m_mouse->delay(195);
+    m_mouse->delay(195.5);
     m_mouse->setWheelSpeeds(0, 0);
 }
 
 void Continue::turnLeft() {
     m_mouse->setWheelSpeeds(5*M_PI, 5*M_PI);
-    m_mouse->delay(195);
+    m_mouse->delay(195.5);
     m_mouse->setWheelSpeeds(0, 0);
 }
 
 void Continue::moveForward() {
     m_mouse->setWheelSpeeds(-10*M_PI, 10*M_PI);
-    m_mouse->delay(420);
+    m_mouse->delay(290);
+    for (int i = 0; i < 5; i += 1) {
+        if (m_mouse->read("A") > 0.9) {
+            break;
+        }
+        m_mouse->delay(10);
+    }
     m_mouse->setWheelSpeeds(0, 0);
+    m_mouse->delay(0);
 }
 
 void Continue::correctErrors() {
-    if (wallFront()) {
-        while (m_mouse->read("A") < 0.7) {
-            m_mouse->setWheelSpeeds(-5, 5);
-            m_mouse->delay(10);
-            m_mouse->setWheelSpeeds(0, 0);
-        }
-        while (m_mouse->read("A") > 0.95) {
-            m_mouse->setWheelSpeeds(5, -5);
-            m_mouse->delay(10);
-            m_mouse->setWheelSpeeds(0, 0);
-        }
+    if (m_mouse->read("B") < 0.3) {
+        // Right
+        m_mouse->setWheelSpeeds(-5, 0);
+        m_mouse->delay(10);
+    }
+    if (m_mouse->read("B") > 0.5) {
+        // Left
+        m_mouse->setWheelSpeeds(0, 5);
+        m_mouse->delay(10);
     }
 }
